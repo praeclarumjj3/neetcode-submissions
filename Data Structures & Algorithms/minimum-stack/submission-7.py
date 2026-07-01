ab39ce77bcb57f512ded0ad04a7e445514cb1d88
Jitesh Jain <jitesh.jj2@gmail.com>
2026-07-01 15:16:31 -0400

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.py
@@ -0,0 +1,28 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.mi = None
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.mi is None or val < self.mi:
+            self.mi = val
+        print(self.mi)
+
+    def pop(self) -> None:
+        el = self.stack.pop()
+        if el == self.mi and len(self.stack) > 0:
+            self.mi = min(self.stack)
+        elif len(self.stack) == 0:
+            self.mi = None
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        print(self.mi)
+        return self.mi
