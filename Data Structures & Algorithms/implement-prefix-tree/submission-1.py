0a3b31d3f2bffb8335dc7286ada67cb697824885
Jitesh Jain <jitesh.jj2@gmail.com>
2026-07-10 15:07:42 -0400

    Update: implement-prefix-tree - submission-1

--- Data Structures & Algorithms/implement-prefix-tree/submission-1.py
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -1,42 +1,26 @@
-class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
-        solutions = []
+class PrefixTree:
 
-        used_cols = set()
-        used_diag1 = set()  # row - col
-        used_diag2 = set()  # row + col
+    def __init__(self):
+        self.graph = {}
+        
 
-        positions = []
+    def insert(self, word: str) -> None:
+        
+        idx = 1
+        while idx <= len(word):
+            w = word[0:idx]
+            if w not in self.graph:
+                self.graph[w] = [word]
+            elif word not in self.graph[w]:
+                self.graph[w].append(word)
+            idx += 1
 
-        def backtrack(row):
-            if row == n:
-                board = []
 
-                for col in positions:
-                    board.append("." * col + "Q" + "." * (n - col - 1))
+    def search(self, word: str) -> bool:
+        return word in self.graph and word in self.graph[word]
+        
 
-                solutions.append(board)
-                return
-
-            for col in range(n):
-                if (
-                    col in used_cols
-                    or row - col in used_diag1
-                    or row + col in used_diag2
-                ):
-                    continue
-
-                positions.append(col)
-                used_cols.add(col)
-                used_diag1.add(row - col)
-                used_diag2.add(row + col)
-
-                backtrack(row + 1)
-
-                positions.pop()
-                used_cols.remove(col)
-                used_diag1.remove(row - col)
-                used_diag2.remove(row + col)
-
-        backtrack(0)
-        return solutions
+    def startsWith(self, prefix: str) -> bool:
+        return prefix in self.graph
+        
+        
