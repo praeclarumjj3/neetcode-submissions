6ba6399f94406f4dd22a4c2af3c02b9004246b83
Jitesh Jain <jitesh.jj2@gmail.com>
2026-07-16 16:54:55 -0400

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reach = 0
+
+        for i, jump in enumerate(nums):
+            # We cannot even reach index i
+            if i > max_reach:
+                return False
+
+            max_reach = max(max_reach, i + jump)
+
+            if max_reach >= len(nums) - 1:
+                return True
+
+        return True
