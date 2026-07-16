4abf8576b9851c541473f6aef52e586b5becb9ff
Jitesh Jain <jitesh.jj2@gmail.com>
2026-07-15 21:40:48 -0400

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        words = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True  # empty string can always be formed
+
+        for end in range(1, len(s) + 1):
+            for start in range(end):
+                if dp[start] and s[start:end] in words:
+                    dp[end] = True
+                    break
+
+        return dp[len(s)]
